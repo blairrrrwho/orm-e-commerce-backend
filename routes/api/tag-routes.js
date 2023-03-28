cd906bc262b63d22b106364acdae475a095e98df
Blair Millet <120157336+blairrrrwho@users.noreply.github.com>
2023-03-28 15:23:36 -0500

    set up routes for post

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -32,8 +32,14 @@ router.get('/:id', async(req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
   // create a new tag
+  try {
+    const data = await Tag.create(req.body)
+    res.status(200).json(data)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 router.put('/:id', (req, res) => {
