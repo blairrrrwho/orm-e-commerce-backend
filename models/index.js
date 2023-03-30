b0334a2304d880730d67a9b3a2fc66ccf8872611
Blair Millet <120157336+blairrrrwho@users.noreply.github.com>
2023-03-29 20:48:35 -0500

    fixed models index.js for product tag - changed belongsToMany

--- models/index.js
+++ models/index.js
@@ -16,7 +16,7 @@ Category.hasMany(Product, {
 })
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsTo(Tag, {
+Product.belongsToMany(Tag, {
   through: {
     model: ProductTag,
     unique: false
